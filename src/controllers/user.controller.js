3df56104cfd373229dc60b15e9ac207db7a585a3
MD Shbab Uddin <your_email@example.com>
2025-11-16 00:14:23 +0530

    Fix refreshAccessToken logic and add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -187,7 +187,7 @@ const logoutUser = asyncHandler(async(req, res) => {
 const refreshAccessToken = asyncHandler(async (req, res) => {
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-    if (incomingRefreshToken) {
+    if (!incomingRefreshToken) {
         throw new ApiError(401, "unauthorized request")
     }
 
@@ -200,7 +200,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
         const user = await User.findById(decodedToken?._id)
     
         if (!user) {
-            throw new ApiError(401, "Invalid refresh token")
+            throw new ApiError(401, "Invalid refresh token")  
         }
     
         if (incomingRefreshToken !== user?.refreshToken) {
